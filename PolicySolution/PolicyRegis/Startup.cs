811deb76fae50b10154e82c23fc30e26cca8e83c
abhayvel <quickitdotnet@gmail.com>
2022-09-05 09:01:36 +0530

    added

--- PolicySolution/PolicyRegis/Startup.cs
+++ PolicySolution/PolicyRegis/Startup.cs
@@ -50,7 +50,7 @@ namespace PolicyRegis
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+					pattern: "{controller=Address}/{action=Index}/{id?}");
 			});
 		}
 	}
