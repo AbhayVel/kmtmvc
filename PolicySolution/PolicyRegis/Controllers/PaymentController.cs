51bc23410bfaa0e9f1042c7feb12389817cf14d6
True-Alpha09 <irishikesh183@gmail.com>
2022-09-15 08:30:43 +0530

    PaymentPageByRishi

---
+++ PolicySolution/PolicyRegis/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PolicyModels;
+using PolicyService;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PolicyRegis.Controllers
+{
+	public class PaymentController : Controller
+	{
+		public IActionResult Index(PymntDtlsSearch pymntDtlsSearch)
+		{
+
+			var adService = new PaymentService();
+			IEnumerable<Payment> lst = adService.GetPaymentList();
+			lst = pymntDtlsSearch.GetWhere(lst);
+			lst = pymntDtlsSearch.GetOrderBy(lst);
+			lst = pymntDtlsSearch.GetPagination(lst);
+			ViewBag.PymntDtlsSearch = pymntDtlsSearch;
+
+			return View(lst.ToList());
+		}
+	}
+}
