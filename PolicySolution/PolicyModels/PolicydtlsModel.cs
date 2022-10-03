aebac24a766b53cf41581a2ba49554c6b7fbf199
RajMVC <rajdevkar16@gmail.com>
2022-10-03 18:52:43 +0530

    updated

---
+++ PolicySolution/PolicyModels/PolicydtlsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolicyModels
+{
+    public class PolicydtlsModel
+    {
+        public String PolicyHoldername { get; set; }
+        public int Recid { get; set; }
+        public int Checkbox { get; set; }
+        public int DOB { get; set; }
+        public int Gender { get; set; }
+        public int PolicyType { get; set; }
+        public int PolicyCatagory { get; set; }
+
+
+
+
+    }
+}
---
+++ PolicySolution/PolicyRegis/Controllers/PolicydtlsModelC.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PolicyRegis.Controllers
+{
+    public class PolicydtlsModelC : Controller
+    {
+        public IActionResult Index( )
+        {
+            return View();
+        }
+    }
+}
